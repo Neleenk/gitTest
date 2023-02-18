88d9ce894bc4bd1d62984255886ecfbaa5e437b8
Neleenk <nehleenrahman@gmail.com>
2023-02-18 14:52:28 -0600

    commited

---
+++ src/launchBrowser/LaunchChrome.java
@@ -0,0 +1,15 @@
+package launchBrowser;
+
+import java.util.Scanner;
+
+public class LaunchChrome {
+	
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
